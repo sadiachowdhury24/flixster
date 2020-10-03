a69836784c477a45608f087683aa5208ecdb7c95
Sadia Chowdhury <sadiachowdhury2000@gmail.com>
2020-10-03 11:13:10 -0400

    Flix Part 2

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Sadia Chowdhury on 10/3/20.
+//  Copyright © 2020 SadiaChowdhury. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
